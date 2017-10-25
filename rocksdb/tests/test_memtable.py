1b1c32ab4a2000d36b217b8386503553a1788815
Louie Lu <git@louie.lu>
2017-10-25 22:14:25 +0800

    Fix unittest

--- rocksdb/tests/test_memtable.py
+++ rocksdb/tests/test_memtable.py
@@ -4,13 +4,20 @@ import pytest
 import shutil
 import os
 
+
+def clean_db():
+    if os.path.exists('/tmp/test'):
+        shutil.rmtree("/tmp/test")
+
 def test_open_skiplist_memtable_factory():
+    clean_db()
     opts = rocksdb.Options()
     opts.memtable_factory = rocksdb.SkipListMemtableFactory()
     opts.create_if_missing = True
     test_db = rocksdb.DB("/tmp/test", opts)
 
 def test_open_vector_memtable_factory():
+    clean_db()
     opts = rocksdb.Options()
     opts.allow_concurrent_memtable_write = False
     opts.memtable_factory = rocksdb.VectorMemtableFactory()
